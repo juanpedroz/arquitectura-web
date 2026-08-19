8699f2f39fe2e1ce7c7a206d3f9dea129648d8a2
Juan Pedro Zugbi <jzugbi@gmail.com>
2026-08-19 20:15:38 -0300

    AbstractFactory

--- ejercicio_uno/Practico1/src/main/java/org/example/factory/AbstractFactory.java
+++ ejercicio_uno/Practico1/src/main/java/org/example/factory/AbstractFactory.java
@@ -1,5 +1,25 @@
 package org.example.factory;
 
+import org.example.dao.ClienteDAO;
+import org.example.dao.ProductoDAO;
+import org.example.dao.FacturaDAO;
+import org.example.dao.FacturaProductoDAO;
+
+
 public class AbstractFactory {
-    
+    public static final int MYSQL_JDBC = 1;
+
+    public abstract ClienteDAO getClienteDAO();
+    public abstract FacturaDAO getFacturaDAO();
+    public abstract ProductoDAO getProductoDAO();
+    public abstract FacturaProducto getFacturaProducto();
+
+    public static AbstractFactory getDAOFactory(int whichFactory) {
+        switch (whichFactory) {
+            case MYSQL_JDBC : {
+                return MySQLDAOFactory.getInstance();
+            }
+            default: return null;
+        }
+    }
 }
