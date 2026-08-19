e1a8ae90444a4d0bcdb5f703c2cafeb5aed0fbd1
Manuel Dualde <manueldualde2012@gmail.com>
2026-08-19 20:47:55 -0300

    fix

--- ejercicio_uno/Practico1/src/main/java/org/example/entities/Cliente.java
+++ ejercicio_uno/Practico1/src/main/java/org/example/entities/Cliente.java
@@ -11,6 +11,6 @@ public class Cliente {
 
     private int idCliente;
     private String nombre;
-    private String email
+    private String email;
 
 }
--- ejercicio_uno/Practico1/src/main/java/org/example/entities/Factura.java
+++ ejercicio_uno/Practico1/src/main/java/org/example/entities/Factura.java
@@ -10,10 +10,4 @@ import lombok.*;
 public class Factura {
     private int idFactura;
     private int idCliente;
-
-    public Factura(int idFactura, int idCliente) {
-        this.idFactura = idFactura;
-        this.idCliente = idCliente;
-    }
-
 }
