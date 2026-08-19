b5a1eab2b181f367c09d15bcb58cb1ceb54845eb
Juan Pedro Zugbi <jzugbi@gmail.com>
2026-08-19 20:36:43 -0300

    Entidades

--- ejercicio_uno/Practico1/src/main/java/org/example/entities/FacturaProducto.java
+++ ejercicio_uno/Practico1/src/main/java/org/example/entities/FacturaProducto.java
@@ -1,5 +1,38 @@
 package org.example.entities;
 
 public class FacturaProducto {
-    
+    private int idFactura;
+    private int idProducto;
+    private int cantidad;
+
+    public FacturaProducto(int idFactura, int idProducto, int cantidad) {
+        this.idFactura = idFactura;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
+    }
+
+    public int getIdFactura() {
+        return idFactura;
+    }
+    public int getIdProducto() {
+        return idProducto;
+    }
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setIdFactura(int idFactura) {
+        this.idFactura = idFactura;
+    }
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("FacturaProducto[idFactura=%d, idProducto=%d, cantidad=%d]", idFactura, idProducto, cantidad);
+    }
 }
