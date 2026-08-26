f6581fc006f81649eeeb15670169b61a3e23f796
TatianaPereyra <pereyratatiana175@gmail.com>
2026-08-26 18:23:50 -0300

    Create Product branch

--- ejercicio_uno/Practico1/src/main/java/org/example/dao/ProductoDAO.java
+++ ejercicio_uno/Practico1/src/main/java/org/example/dao/ProductoDAO.java
@@ -1,5 +1,21 @@
 package org.example.dao;
 
+import org.example.entities.Producto;
+
+import java.sql.Connection;
+import java.util.List;
+
 public class ProductoDAO {
+    Connection connection;
+
+    public ProductoDAO(Connection conn){
+        this.connection = conn;
+    }
+
+    public List<Producto> getAllProducts(){
+        return null;
+    }
+
+
     
 }
