44bc974ca63eabbca60a9d3e3057dcfa2895db15
Juan Pedro Zugbi <jzugbi@gmail.com>
2026-08-26 19:39:41 -0300

    Implementción Métodos y funciones de la clase ClienteDAO

--- ejercicio_uno/Practico1/src/main/java/org/example/dao/ClienteDAO.java
+++ ejercicio_uno/Practico1/src/main/java/org/example/dao/ClienteDAO.java
@@ -1,13 +1,141 @@
 package org.example.dao;
 
-import java.sql.Connection;
+import org.example.entities.Cliente;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClienteDAO {
     private Connection connection;
 
     public ClienteDAO(Connection connection){
         this.connection = connection;
+    }
+
+    public List<Cliente> getAll(){
+        String query = "SELECT * FROM Cliente";
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            ps = connection.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                clientes.add(new Cliente(
+                        rs.getInt("idCliente"),
+                        rs.getString("nombre"),
+                        rs.getString("email")));
+            }
+            return clientes;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public boolean insert (Cliente cliente){
+        String query = "INSERT INTO Cliente (idCliente, nombre, email) VALUES (?,?,?)";
+        PreparedStatement ps = null;
+
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, cliente.getIdCliente());
+            ps.setString(2, cliente.getNombre());
+            ps.setString(3, cliente.getEmail());
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     }
-    
+
+    public boolean delete(int idCliente){
+        String query = "DELETE FROM Cliente WHERE idCliente = ?";
+        PreparedStatement ps = null;
+
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, idCliente);
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (ps != null){
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public Cliente find(int idCliente){
+        String query = "SELECT * FROM Cliente WHERE idCliente = ?";
+        Cliente cliente = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, idCliente); //seteo el cliente que voy a buscar
+            rs = ps.executeQuery(); //ejecuto la consulta
+            if (rs.next()) {
+                return new Cliente(
+                        rs.getInt("idCliente"),
+                        rs.getString("nombre"),
+                        rs.getString("email"));
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (ps != null){
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public boolean update(Cliente cliente){
+        String query = "UPDATE Cliente SET nombre = ?, email = ? WHERE idCliente = ?";
+        PreparedStatement ps = null;
+
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, cliente.getNombre());
+            ps.setString(2, cliente.getEmail());
+            ps.setInt(3, cliente.getIdCliente());
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (ps != null){
+                try {
+                    ps.close();
+                } catch (SQLException e) {}
+            }
+        }
+
+    }
 }
