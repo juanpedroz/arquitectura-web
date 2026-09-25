6c65c3ac50fbfc603802f0197dbfe81b09cee670
Juan Pedro Zugbi <jzugbi@gmail.com>
2026-09-24 23:41:39 -0300

    feat: crear proyecto Spring Boot en ejercicio_tres

---
+++ ejercicio_tres/src/main/java/com/arqweb/integrador_tres/IntegradorTresApplication.java
@@ -0,0 +1,13 @@
+package com.arqweb.integrador_tres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradorTresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegradorTresApplication.class, args);
+    }
+
+}
---
+++ ejercicio_tres/src/test/java/com/arqweb/integrador_tres/IntegradorTresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arqweb.integrador_tres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorTresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
